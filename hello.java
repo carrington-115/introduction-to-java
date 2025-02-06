712417081e2eacf1c4e200efc937c289f28fd1e5
carrington-115 <frumarkcarrington@gmail.com>
2025-02-06 07:43:51 +0530

    wrote new function and added a user input functionality through the new function

--- hello.java
+++ hello.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 class QuickStart {
+
+    public void userInput() {
+        Scanner sc = new Scanner(System.in);
+        String scanned = sc.next();
+        System.out.println(scanned);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        QuickStart obj = new QuickStart();
+
+        int number = 4;
+        String name = "mark";
+        System.out.println("Hello, World." + number);
+        System.out.println("My name is: " + name);
+        obj.userInput();
     }
 }
