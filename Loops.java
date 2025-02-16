de626d17cf47b3be4952bce903ad99ad3a382183
carrington-115 <frumarkcarrington@gmail.com>
2025-02-16 19:55:56 +0530

    built a program to fill an array of string with names

--- Loops.java
+++ Loops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Loops {
     public String[] userAccountNames() {
         String[] names = { "Fru", "Mark", "Carrington", "Chei" };
@@ -6,16 +8,27 @@ public class Loops {
 }
 
 class ExecuteFile {
+    void add_name(String[] names, int position) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        names[position] = name;
+    }
+
     public static void main(String[] args) {
-        Loops loopObj = new Loops();
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello world");
-        String[] names = loopObj.userAccountNames();
+        String[] names = new String[5];
+        ExecuteFile executor = new ExecuteFile();
+
+        // populating names
         for (int i = 0; i < names.length; i++) {
-            System.out.println(i + ": " + names[i]);
+            executor.add_name(names, i);
         }
 
-        for (String element : names) {
-            System.out.println(element);
+        // print out the names
+        for (String name : names) {
+            System.out.println(name);
         }
+        sc.close();
     }
 }
